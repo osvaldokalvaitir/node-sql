e68912ffbee27cd87d2eab514f307f3a06273f67
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2020-02-29 13:12:55 -0300

    Projeto concluído

---
+++ src/controllers/ReportController.js
@@ -0,0 +1,36 @@
+const { Op } = require('sequelize');
+const User = require('../models/User');
+
+module.exports = {
+  async show(req, res) {
+    // Encontrar todos os usuários que tem email que termina com @rocketseat.com.br
+    // Desses usuários eu quero buscar todos que moram na rua "Rua Coronel Macedo"
+    // Desses usuários eu quero buscar as tecnologias que começam com React
+
+    const users = await User.findAll({
+      attributes: ['name', 'email'],
+      where: {
+        email: {
+          [Op.iLike]: '%@outlook.com'
+        }
+      },
+      include: [
+        {
+          association: 'addresses',
+          where: { street: 'Rua Coronel Macedo' }
+        },
+        {
+          association: 'techs',
+          required: false,
+          where: {
+            name: {
+              [Op.iLike]: 'React%'
+            }
+          }
+        },
+      ]
+    })
+
+    return res.json(users);
+  }
+}
